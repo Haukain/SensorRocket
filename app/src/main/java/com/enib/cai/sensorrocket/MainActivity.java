c9cac78b9c92478730e542f787980abd075ffc2f
Haukain <Haukain@users.noreply.github.com>
2018-10-16 11:01:55 +0200

    Initial commit

---
+++ app/src/main/java/com/enib/cai/sensorrocket/MainActivity.java
@@ -0,0 +1,12 @@
+package com.enib.cai.sensorrocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
