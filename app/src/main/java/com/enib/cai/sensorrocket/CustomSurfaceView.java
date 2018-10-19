3f95ae7a8eedd74d99823338981a778f8b65fac6
Haukain <Haukain@users.noreply.github.com>
2018-10-19 20:42:31 +0200

    Boutons menus

--- app/src/main/java/com/enib/cai/sensorrocket/CustomSurfaceView.java
+++ app/src/main/java/com/enib/cai/sensorrocket/CustomSurfaceView.java
@@ -17,35 +17,49 @@ import java.util.Vector;
 
 public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Callback,Runnable{
 
+    // Thread related
     private Thread mThread;
     private boolean mRunning;
 
+    // Time
     private long mBeginningTime;
     private long mCurrentTime;
     private int mCurrentTimeInSecond;
+    private long mTimeInPause;
 
+    // Ennemy spawning
     private int mEnnemySpawnCountdown;
     private long mlastEnnemySpawn;
 
+    // Scene Elements
     private Rocket mRocket;
     private Vector<Point> mRocketSmoke;
     private Vector<Ennemy> mEnnemy;
     private int mbgColor;
     private boolean mRocketPosUnset;
 
+    // On touch hitbox
     private Region mTouchHitBox;
 
+    // Sensors
     private Accelero mAccelero;
     //private Gyro mGyro;
     private Lumen mLumen;
 
+    // Holder for canvas
     private SurfaceHolder mSurfaceHolder;
+
+    //Pause Flag
+    private boolean mPaused;
 
     public CustomSurfaceView (Context context){
         super(context);
 
+        mPaused = false;
+        mTimeInPause = 0;
+
         mBeginningTime = System.currentTimeMillis();
-        mCurrentTime = mBeginningTime;
+        mCurrentTime = 0;
         mEnnemySpawnCountdown=2000;
         mlastEnnemySpawn=0;
 
@@ -67,6 +81,7 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
 
     }
 
+    // Activity onPause
     public void onPause()
     {
         mAccelero.onPause();
@@ -81,6 +96,7 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         }
     }
 
+    // Activity onResume
     public void onResume() {
         mAccelero.onResume();
         //mGyro.onResume();
@@ -90,122 +106,122 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         mThread.start();
     }
 
+    // Thread running loop
     @Override
     public void run() {
         Canvas canvas;
         while (mRunning) {
 
-            // Setting the rocket position for the first time
-            if(mRocketPosUnset)
-            {
-                if(this.getWidth()!=0) {
-                    mRocket.setPosition(new Point(this.getWidth() / 2, this.getHeight() - 250));
-                    mRocketPosUnset = false;
-                }
-            }
-            // Making sure we don't run any loop while the rocket has no position
-            else {
-                mCurrentTime = System.currentTimeMillis() - mBeginningTime;
-                mCurrentTimeInSecond = (int)mCurrentTime/1000;
-
-                // Random test to see if we should add an ennemy to the ennemy vector
-                if( (mCurrentTime-mlastEnnemySpawn)>(mEnnemySpawnCountdown/(1 + mCurrentTimeInSecond/10)) )
-                {
-                    mEnnemy.add(new Ennemy(this.getWidth()));
-                    mlastEnnemySpawn=mCurrentTime;
-                }
-
-                // Drawing loop
-                if (mSurfaceHolder.getSurface().isValid()) {
-                    canvas = mSurfaceHolder.lockCanvas();
-
-                    //Painting background
-                    Paint bgPaint = new Paint();
-                    bgPaint.setColor(mbgColor);
-                    canvas.drawPaint(bgPaint);
-
-                    //Drawing animation
-                    Paint smokePaint = new Paint();
-                    smokePaint.setColor(Color.argb((int)(Math.random()*50 +50),255,255,255));
-                    if(mRocketSmoke.size()<20) mRocketSmoke.add(new Point(mRocket.getPosition().x,mRocket.getPosition().y+30));
-                    Iterator<Point> pi = mRocketSmoke.iterator();
-                    while(pi.hasNext())
-                    {
-                        Point p = pi.next();
-                        canvas.drawCircle(p.x,p.y,(int)(Math.random()*25+20),smokePaint);
-                        if(p.y>this.getHeight()+50) pi.remove();
-                        else p.set(p.x + (int)(Math.random()*10-5),p.y+20);
+            if(!mPaused) {
+                // Setting the rocket position for the first time
+                if (mRocketPosUnset) {
+                    if (this.getWidth() != 0) {
+                        mRocket.setPosition(new Point(this.getWidth() / 2, this.getHeight() - 250));
+                        mRocketPosUnset = false;
                     }
+                }
+                // Making sure we don't run any loop while the rocket has no position
+                else {
+                    // Drawing loop
+                    if (mSurfaceHolder.getSurface().isValid()) {
+                        canvas = mSurfaceHolder.lockCanvas();
 
-                    //Getting Rocket Path
-                    Path rocketPath = mRocket.drawRocket();
-                    //Getting Rocket Hitbox
-                    Region rocketHitBox = new Region();
-                    rocketHitBox.setPath(rocketPath,new Region(0,0,this.getWidth(),this.getHeight()));
-                    //Drawing Rocket
-                    canvas.drawPath(rocketPath, mRocket.getPaint());
+                        // Updating Time
+                        mCurrentTime = System.currentTimeMillis() - mBeginningTime - mTimeInPause;
+                        mCurrentTimeInSecond = (int) mCurrentTime / 1000;
 
-                    //Updating the rocket position
-                    float acceleroOffset = Math.abs(mAccelero.x)>0.2?(-mAccelero.x*3):0;
-                    float newX = mRocket.getPosition().x + acceleroOffset;
-                    if (newX > this.getWidth()) newX = this.getWidth();
-                    else if (newX < 0) newX = 0;
-                    mRocket.setPosition(new Point((int) newX, mRocket.getPosition().y));
+                        // Testing if ennemy should appear
+                        if ((mCurrentTime - mlastEnnemySpawn) > (mEnnemySpawnCountdown / (1 + mCurrentTimeInSecond / 10))) {
+                            mEnnemy.add(new Ennemy(this.getWidth()));
+                            mlastEnnemySpawn = mCurrentTime;
+                        }
 
-                    boolean hitThisLoop = false;
+                        //Painting background
+                        Paint bgPaint = new Paint();
+                        bgPaint.setColor(mbgColor);
+                        canvas.drawPaint(bgPaint);
 
-                    //Iterating the ennemy vector
-                    Iterator<Ennemy> it = mEnnemy.iterator();
-                    while (it.hasNext()) {
-                        Ennemy e = it.next();
+                        //Drawing animation
+                        Paint smokePaint = new Paint();
+                        smokePaint.setColor(Color.argb((int) (Math.random() * 50 + 50), 255, 255, 255));
+                        if (mRocketSmoke.size() < 20)
+                            mRocketSmoke.add(new Point(mRocket.getPosition().x, mRocket.getPosition().y + 30));
+                        Iterator<Point> pi = mRocketSmoke.iterator();
+                        while (pi.hasNext()) {
+                            Point p = pi.next();
+                            canvas.drawCircle(p.x, p.y, (int) (Math.random() * 25 + 20), smokePaint);
+                            if (p.y > this.getHeight() + 50) pi.remove();
+                            else p.set(p.x + (int) (Math.random() * 10 - 5), p.y + 20);
+                        }
 
-                        //Getting Ennemy Path
-                        Path ennemyPath = e.drawEnnemy();
-                        //Getting Ennemy Hitbox
-                        Region ennemyHitBox = new Region();
-                        ennemyHitBox.setPath(ennemyPath,new Region(0,0,this.getWidth(),this.getHeight()));
+                        //Getting Rocket Path
+                        Path rocketPath = mRocket.drawRocket();
+                        //Getting Rocket Hitbox
+                        Region rocketHitBox = new Region();
+                        rocketHitBox.setPath(rocketPath, new Region(0, 0, this.getWidth(), this.getHeight()));
+                        //Drawing Rocket
+                        canvas.drawPath(rocketPath, mRocket.getPaint());
 
-                        if(!hitThisLoop) {
-                            //Checking for collision only if not hit this loop
-                            if (!rocketHitBox.quickReject(ennemyHitBox) && rocketHitBox.op(ennemyHitBox, Region.Op.INTERSECT)) {
-                                //If not alread hit and currently hit, set "hit" to 1
-                                if (!mRocket.getHit()) {
-                                    hitThisLoop=true;
-                                    mRocket.setHit(true);
+                        //Updating the rocket position
+                        float acceleroOffset = Math.abs(mAccelero.x) > 0.2 ? (-mAccelero.x * 3) : 0;
+                        float newX = mRocket.getPosition().x + acceleroOffset;
+                        if (newX > this.getWidth()) newX = this.getWidth();
+                        else if (newX < 0) newX = 0;
+                        mRocket.setPosition(new Point((int) newX, mRocket.getPosition().y));
+
+                        boolean hitThisLoop = false;
+
+                        //Iterating the ennemy vector
+                        Iterator<Ennemy> it = mEnnemy.iterator();
+                        while (it.hasNext()) {
+                            Ennemy e = it.next();
+
+                            //Getting Ennemy Path
+                            Path ennemyPath = e.drawEnnemy();
+                            //Getting Ennemy Hitbox
+                            Region ennemyHitBox = new Region();
+                            ennemyHitBox.setPath(ennemyPath, new Region(0, 0, this.getWidth(), this.getHeight()));
+
+                            if (!hitThisLoop) {
+                                //Checking for collision only if not hit this loop
+                                if (!rocketHitBox.quickReject(ennemyHitBox) && rocketHitBox.op(ennemyHitBox, Region.Op.INTERSECT)) {
+                                    //If not alread hit and currently hit, set "hit" to 1
+                                    if (!mRocket.getHit()) {
+                                        hitThisLoop = true;
+                                        mRocket.setHit(true);
+                                    }
                                 }
+                            }
+
+                            if (!mTouchHitBox.quickReject(ennemyHitBox) && mTouchHitBox.op(ennemyHitBox, Region.Op.INTERSECT)) {
+                                //Hit Ennemy if touched
+                                e.setHit(true);
+                            }
+                            if (mLumen.currentLux > 1000) {
+                                e.setHit(true);
+                            }
+
+                            //Drawing the Ennemy
+                            canvas.drawPath(ennemyPath, e.getPaint());
+
+                            //Updating the ennemy position
+                            e.setPosition(new Point(e.getPosition().x, e.getPosition().y + e.getSpeed()));
+                            if (e.getPosition().y > (this.getHeight() + 10)) {
+                                //removing ennemy if it is out of bounds
+                                it.remove();
                             }
                         }
 
-                        if (!mTouchHitBox.quickReject(ennemyHitBox) && mTouchHitBox.op(ennemyHitBox, Region.Op.INTERSECT)) {
-                            //Hit Ennemy if touched
-                            e.setHit(true);
-                        }
-                        if(mLumen.currentLux>1000)
+                        if (!hitThisLoop && mRocket.getHit()) // If not hit by any ennemy and already hit, set "hit" to 0
                         {
-                            e.setHit(true);
+                            mRocket.setHit(false);
                         }
 
-                        //Drawing the Ennemy
-                        canvas.drawPath(ennemyPath,e.getPaint());
-
-                        //Updating the ennemy position
-                        e.setPosition(new Point(e.getPosition().x, e.getPosition().y + e.getSpeed()));
-                        if (e.getPosition().y > (this.getHeight() + 10)) {
-                            //removing ennemy if it is out of bounds
-                            it.remove();
-                        }
-                    }
-
-                    if(!hitThisLoop && mRocket.getHit()) // If not hit by any ennemy and already hit, set "hit" to 0
-                    {
-                        mRocket.setHit(false);
-                    }
-
-                    //Drawing text
-                    Paint textPaint = new Paint();
-                    textPaint.setColor(Color.WHITE);
-                    textPaint.setTextSize(80);
-                    canvas.drawText("SCORE : " + String.valueOf(mCurrentTimeInSecond),this.getWidth()/2,100,textPaint);
+                        //Drawing text
+                        Paint textPaint = new Paint();
+                        textPaint.setColor(Color.WHITE);
+                        textPaint.setTextSize(80);
+                        canvas.drawText("SCORE : " + String.valueOf(mCurrentTimeInSecond), this.getWidth() / 2, 100, textPaint);
                     /*
                     canvas.drawText("Acceleration X : " + String.format("%.1f", mAccelero.x), 50, 100, textPaint);
                     canvas.drawText("Acceleration Y : " + String.format("%.1f", mAccelero.y), 50, 200, textPaint);
@@ -217,12 +233,18 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
                     canvas.drawText("nb Ennemies"+String.valueOf(mEnnemy.size()),500,200,textPaint);
                     */
 
-                    mSurfaceHolder.unlockCanvasAndPost(canvas);
+                        mSurfaceHolder.unlockCanvasAndPost(canvas);
+                    }
                 }
+            }
+            else
+            {
+                mTimeInPause = System.currentTimeMillis() - mBeginningTime - mCurrentTime;
             }
         }
     }
 
+    // Implementing onTouch reaction
     @Override
     public boolean onTouchEvent(MotionEvent e)
     {
@@ -234,11 +256,35 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
             Path touchPath = new Path();
             touchPath.addCircle(x,y,50, Path.Direction.CW);
 
+            // Creating hitbox to collide with ennemies
             mTouchHitBox.setPath(touchPath,new Region(0,0,this.getWidth(),this.getHeight()));
 
             return true;
         }
         return false;
+    }
+
+    public void onPauseClick()
+    {
+        mPaused=!mPaused;
+    }
+
+    public void onRestartClick()
+    {
+        mPaused = false;
+        mTimeInPause = 0;
+
+        mBeginningTime = System.currentTimeMillis();
+        mCurrentTime = 0;
+        mEnnemySpawnCountdown=2000;
+        mlastEnnemySpawn=0;
+
+        mRocket = new Rocket();
+        mRocketSmoke = new Vector<>();
+        mEnnemy = new Vector<>();
+        //mbgColor = Color.argb(255,135, 206, 235);
+        mbgColor = Color.argb(255,18, 62, 135);
+        mRocketPosUnset = true;
     }
 
     @Override
--- app/src/main/java/com/enib/cai/sensorrocket/MainActivity.java
+++ app/src/main/java/com/enib/cai/sensorrocket/MainActivity.java
@@ -1,32 +1,79 @@
 package com.enib.cai.sensorrocket;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CustomSurfaceView mSurfaceView;
+    private FrameLayout mSceneLayout;
+
+    private LinearLayout mGameLayout;
+
+    private LinearLayout mButtonLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSurfaceView = new CustomSurfaceView(this);
-        mSurfaceView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-        setContentView(mSurfaceView);
+
+        mSceneLayout = new FrameLayout(this);
+
+        mGameLayout = new LinearLayout(this);
+
+        CustomSurfaceView gameView = new CustomSurfaceView(this);
+        gameView.setId(1);
+        mGameLayout.addView(gameView);
+
+        mButtonLayout = new LinearLayout(this);
+
+        final Context that = this;
+        Button pauseButton = new Button(this);
+        pauseButton.setText("Pause");
+        pauseButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ((CustomSurfaceView)mGameLayout.findViewById(1)).onPauseClick();
+                    }
+                }
+        );
+        Button restartButton = new Button(this);
+        restartButton.setText("Restart");
+        restartButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ((CustomSurfaceView)mGameLayout.findViewById(1)).onRestartClick();
+                    }
+                }
+        );
+        mButtonLayout.addView(pauseButton);
+        mButtonLayout.addView(restartButton);
+
+        mSceneLayout.addView(mGameLayout);
+        mSceneLayout.addView(mButtonLayout);
+
+        mSceneLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        setContentView(mSceneLayout);
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
-        mSurfaceView.onPause();
+        ((CustomSurfaceView)mGameLayout.findViewById(1)).onPause();
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        mSurfaceView.onResume();
+        ((CustomSurfaceView)mGameLayout.findViewById(1)).onResume();
     }
 }
